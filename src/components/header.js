882e0bd5cc8d820d9c559cb9ba925bd60a6324c3
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-03-13 04:00:52 +0530

    Logo

--- src/components/header.js
+++ src/components/header.js
@@ -21,11 +21,11 @@ const Header = () => {
                             <a href="#" className="-m-1.5 p-1.5">
                                 <span className="sr-only">Your Company</span>
                                 <img
-                                    className="h-8 w-auto"
-                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                    className="h-16 w-auto"
+                                    src="https://i.ibb.co/K0L2FL9/Covid-Live.png"
                                     alt=""
-                                />
-                            </a>
+                                    />
+                                 </a>
                         </div>
                         <div className="flex lg:hidden">
                             <button
