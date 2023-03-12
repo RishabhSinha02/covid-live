fdf340eb06101fbf71768459fe9df908bdd3afd4
Rishabh Sinha <sinharishabh2002@gmail.com>
2023-03-13 03:34:21 +0530

    Landing Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Covid from './components/covid'
+import Header from './components/header'
 
 const App =() =>{
   return(
     <>
-      <h1>Covid 19 Corona Virus Tracker</h1>
+      <Header/>
       <Covid />
     </>
   )
--- src/components/covid.js
+++ src/components/covid.js
@@ -1,10 +1,9 @@
-import React, {useEffect, useState} from 'react'
-
+import React, { useEffect, useState } from 'react'
 const Covid = () => {
 
     const [data, setData] = useState([]);
 
-    const getCovidData = async ()=>{
+    const getCovidData = async () => {
         try {
             const res = await fetch("https://data.covid19india.org/data.json");
             const actualData = await res.json();
@@ -17,17 +16,35 @@ const Covid = () => {
 
     }
     useEffect(() => {
-      getCovidData();
+        getCovidData();
     }, []);
-    
+
+    const stats = [
+        { id: 1, name: 'Our Country', value: 'India' },
+        { id: 2, name: 'Total Recovered', value: data.deltarecovered },
+        { id: 3, name: 'Total Confirmed', value: data.deltaconfirmed },
+        { id: 4, name: 'Total Deaths', value: data.deltadeaths },
+        { id: 5, name: 'Total Actives', value: data.active },
+        { id: 6, name: 'Last Update', value: data.lastupdatedtime},
+        // { id: 6, name: 'Last Update', value: data.lastupdatedtime.split(" ")[0]},
+      ]
+
     return (
         <>
-            {/* <div></div> */}
-            <div>Total Recovered: {data.deltarecovered}</div>
-            <div>Total Confirmed: {data.deltaconfirmed}</div>
-            <div>Total Deaths: {data.deltadeaths}</div>
-            <div>Total Actives: {data.active}</div>
-            <div>Last Update: {data.lastupdatedtime}</div>
+        <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <dl className="grid grid-cols-1 gap-y-16 gap-x-8 text-center lg:grid-cols-3">
+          {stats.map((stat) => (
+            <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
+              <dt className="text-base leading-7 text-gray-600">{stat.name}</dt>
+              <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                {stat.value}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </div>
         </>
     )
 }
